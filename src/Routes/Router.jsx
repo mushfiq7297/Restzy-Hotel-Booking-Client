751d344d20f815447240fe544f5990efc2bc593f
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-05-10 23:51:18 +0600

    project setup done

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+    ]
+    },
+  ]);
